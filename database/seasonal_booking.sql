e334aaa8e399feaae6900a8cba7c105f35ca5408
gdcrocx <georgejosephdavis@gmail.com>
2016-05-16 15:35:26 +0530

    Removing payment gateway table creation

--- database/seasonal_booking.sql
+++ database/seasonal_booking.sql
@@ -1,9 +1,3 @@
-CREATE TABLE `payment_method` (
-  `payment_method_id` int(11) NOT NULL,
-  `payment_method_desc` varchar(500) DEFAULT NULL,
-  `payment_method_redirect_url` varchar(500) NOT NULL,
-  PRIMARY KEY (`payment_method_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Contains the master payment methods available for the app';
 CREATE TABLE `seasonal_booking` (
   `seasonal_booking_parking_id` int(11) NOT NULL,
   `seasonal_booking_vendor_id` int(11) NOT NULL,

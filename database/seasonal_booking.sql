552553c2b22cf166ab7efa738562d99c6a7c8b38
gdcrocx <georgejosephdavis@gmail.com>
2016-05-19 01:35:41 +0000

    FK Issue resolved - uservehicle should be referenced instead of user_car_details

--- database/seasonal_booking.sql
+++ database/seasonal_booking.sql
@@ -14,6 +14,6 @@ CREATE TABLE `seasonal_booking` (
   KEY `payment_method_id_FK_idx` (`seasonal_booking_payment_method_id`),
   CONSTRAINT `seasonal_booking_parking_id_FK` FOREIGN KEY (`seasonal_booking_parking_id`) REFERENCES `parking` (`parking_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `seasonal_booking_payment_method_id_FK` FOREIGN KEY (`seasonal_booking_payment_method_id`) REFERENCES `payment_method` (`payment_method_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `seasonal_booking_user_car_id_FK` FOREIGN KEY (`seasonal_booking_user_car_id`) REFERENCES `user_car_details` (`car_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `seasonal_booking_user_car_id_FK` FOREIGN KEY (`seasonal_booking_user_car_id`) REFERENCES `uservehicle` (`uservehicle_car_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `seasonal_booking_vendor_id_FK` FOREIGN KEY (`seasonal_booking_vendor_id`) REFERENCES `vendor` (`vendor_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;

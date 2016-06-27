646d6a8edc8c14f0958e6c0a020cf40bdebff4a4
gdcrocx <georgejosephdavis@gmail.com>
2016-06-27 07:08:46 +0530

    Added user_password and user_is_vendor columns for authentication and access control

--- database/user.sql
+++ database/user.sql
@@ -1,8 +1,11 @@
 CREATE TABLE `user` (
-  `user_id` int(11) NOT NULL,
-  `user_name` varchar(45) NOT NULL,
-  `user_phone_no` varchar(45) NOT NULL,
-  `user_first_name` varchar(45) NOT NULL,
-  `user_last_name` varchar(45) NOT NULL,
+  `user_id` int(10) NOT NULL,
+  `user_name` varchar(15) NOT NULL,
+  `user_phone_no` varchar(10) NOT NULL,
+  `user_first_name` varchar(25) NOT NULL,
+  `user_last_name` varchar(25) NOT NULL,
+  `user_email_id` varchar(100) NOT NULL,
+  `user_password` varchar(32) NOT NULL,
+  `user_is_vendor` int(11) NOT NULL,
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Contains user details';

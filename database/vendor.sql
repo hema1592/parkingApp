7e2de3c2ff4a32d1a0d9cfe771614105695a0870
gdcrocx <georgejosephdavis@gmail.com>
2016-06-27 07:15:45 +0530

    Removed vendor_phone_no and vendor_name. Added column vendor_location_id.
    Removed vendor_phone_no and vendor_name as vendor is also a user in `user` table. Added column vendor_location_id.

--- database/vendor.sql
+++ database/vendor.sql
@@ -6,3 +6,8 @@ CREATE TABLE `vendor` (
   `vendor_phone_no` varchar(45) NOT NULL,
   PRIMARY KEY (`vendor_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Contains vendor details';
+
+ALTER TABLE `parkingapp`.`vendor` 
+DROP COLUMN `vendor_phone_no`,
+DROP COLUMN `vendor_name`,
+ADD COLUMN `vendor_location_id` VARCHAR(100) NOT NULL AFTER `vendor_address`;

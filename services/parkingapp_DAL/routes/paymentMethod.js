44c7eddf8c76c880a7410e2b56c735b0456c3c20
gdcrocx <georgejosephdavis@gmail.com>
2016-07-09 21:18:38 +0530

    Removed paymentMethod to keep up the service conventions

--- services/parkingapp_DAL/routes/paymentMethod.js
+++
@@ -1,6 +0,0 @@
-var router = require('express').Router(),
-    service = require('../services/paymentMethod');
-
-router.get('/:method_desc/:method_redirect_url', service.addPaymentMethodDetails);
-
-module.exports = router;
